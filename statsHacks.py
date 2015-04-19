9c88e45f1ee6017ddf5017f1965209a0ffc264e7
Ikaro Silva <ikarosilva@yahoo.com>
2015-04-19 11:31:36 -0400

    added bayesian estimation of binomial dist

--- statsHacks.py
+++ statsHacks.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 def effectSize(N1,N2,t):
     "Calculate D prime. Typical values for small, medium, and large are 0.2 0.5 0.8"
@@ -6,4 +7,9 @@ def effectSize(N1,N2,t):
 
 def SampleSize(t,d):
     "Estimates required sample size for given d-prime and t statistic"
-    return math.sqrt(2)/( (d/t)**2 )
+    return 2.0/( (d/t)**2 )
+
+def estimateTheta(N,k,prior=np.arange(0,1,0.01)):
+    "Use Baye's rule to estimate proportion of sucess from measurement"
+    likelihood=np.power(prior,k)*np.power(1-prior,N-k);
+    return prior[likelihood.argmax()]
