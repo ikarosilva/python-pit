8931704864e2d2034c4f88c013ac91ccab3011b8
Silva, Ikaro <ikaro.silva@philips.com>
2017-11-03 12:14:30 -0400

    interface clean up

--- deep/sims.py
+++ deep/sims.py
@@ -6,6 +6,7 @@ Created on Nov 2, 2017
 from gym import Env
 import numpy as np
 import numpy.random as rnd
+from dateutil import zoneinfo
 
 class MarkovDecisionProcess(Env):
     def __init__(self,T,R,possible_actions):
@@ -13,20 +14,43 @@ class MarkovDecisionProcess(Env):
         self.R=R
         s,a,_=T.shape
         self.number_of_states=s
-        self.number_of_actions=s
+        self.number_of_actions=a
         self.possible_actions=possible_actions
+        self.s=0
         
-    def _step(self, action): 
-        raise NotImplementedError
+    def _step(self, action):  
+        done=False
+        sp = rnd.choice(range(self.number_of_states),p=self.T[self.s,action])
+        reward=self.R[self.s,action,sp]
+        self.s=sp
+        info=''
+        return self.s,reward,done,info
     
     def _reset(self): 
-        raise NotImplementedError
+        self.s=0
     
     def _render(self, mode='human', close=False): 
         return
     
     def _seed(self, seed=None): 
         return []
+    
+    def sample(self):
+        return rnd.choice(self.possible_actions[self.s])
+        
+    def Q_value_iteration(self,discount_rate=0.95,n_iterations=100):
+        Q=np.full((self.number_of_states,self.number_of_actions),-np.inf)
+        for state, actions in enumerate(self.possible_actions):
+            Q[state,actions]=0.0
+        for _ in range(n_iterations):
+            Q_prev=Q.copy()
+            for s in range(self.number_of_states):
+                for a in self.possible_actions[s]:
+                    Q[s,a]= np.sum([
+                                    self.T[s,a,sp]*(self.R[s,a,sp] + discount_rate*np.max(Q_prev[sp]))
+                                    for sp in range(3)
+                        ])
+        return Q
 
 def MDP_1():
     T=np.array([#s a s'
@@ -42,29 +66,15 @@ def MDP_1():
     possible_actions=[[0,1,2],[0,2],[1]]
     return MarkovDecisionProcess(T,R,possible_actions)
 
-def Q_value_iteration(env,discount_rate=0.95,n_iterations=100):
-    Q=np.full((env.number_of_states,env.number_of_actions),-np.inf)
-    for state, actions in enumerate(env.possible_actions):
-        Q[state,actions]=0.0
-    for _ in range(n_iterations):
-        Q_prev=Q.copy()
-        for s in range(env.number_of_states):
-            for a in env.possible_actions[s]:
-                Q[s,a]= np.sum([
-                                env.T[s,a,sp]*(env.R[s,a,sp] + discount_rate*np.max(Q_prev[sp]))
-                                for sp in range(3)
-                    ])
-    return Q
 
 def Q_value_learning(env,discount_rate=0.95,learning_rate0=0.05,learning_rate_decay=0.1,n_iterations=20000):
-    s=0
+    s=env.reset()
     Q=np.full((env.number_of_states,env.number_of_actions),-np.inf)
     for state, actions in enumerate(env.possible_actions):
         Q[state,actions]=0.0
     for iteration in range(n_iterations):
-        a =rnd.choice(env.possible_actions[s])
-        sp = rnd.choice(range(env.number_of_states),p=env.T[s,a])
-        reward=env.R[s,a,sp]
+        a = env.sample()
+        sp,reward,_,_ = env.step(a)
         learning_rate =learning_rate0 / (1+ iteration*learning_rate_decay)
         Q[s,a]= learning_rate*Q[s,a] + (1-learning_rate)*(reward + discount_rate*np.max(Q[sp]))
         s=sp
@@ -72,7 +82,7 @@ def Q_value_learning(env,discount_rate=0.95,learning_rate0=0.05,learning_rate_de
             
 if __name__ == '__main__':
     ex1=MDP_1()
-    Q=Q_value_iteration(ex1,discount_rate=0.95)
+    Q=ex1.Q_value_iteration(discount_rate=0.95)
     print Q 
     print np.argmax(Q,axis=1)   
     
